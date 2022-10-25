6218d85e0109c52f65632b0b4fd0da0f18a1f713
ChanDara-97 <chandara.leav@niptict.edu.kh>
2022-10-25 10:30:33 +0700

    hha

--- src/core/components/HAHA/index.jsx
+++ src/core/components/HAHA/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export function HAHA() {
+  const haha = "skdjfasdf";
+  const jj = () => {
+    return "ah jm,kdmv,jkj,";
+  };
+
   return (
     <div>
       <p>Sophanon is bake tnam for real</p>
     </div>
-  )
+  );
 }
